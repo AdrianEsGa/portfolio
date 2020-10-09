919dd727e4e0f7c9ff95571c0934c325b848aacb
Adrián Estévez <Adrianestevezgayoso@gmail.com>
2020-10-09 13:25:54 +0200

    add firebase service data access

---
+++ src/app/interfaces/team-info.interface.ts
@@ -0,0 +1,6 @@
+export interface TeamInfoInterface{
+  name?: string;
+  job?: string;
+  description?: string;
+  twitter?: string;
+}
--- src/app/services/page-info.service.ts
+++ src/app/services/page-info.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {PageInfoInterface} from '../interfaces/page-info.interface';
+import {TeamInfoInterface} from '../interfaces/team-info.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +10,29 @@ import {PageInfoInterface} from '../interfaces/page-info.interface';
 export class PageInfoService {
 
   info: PageInfoInterface = {};
+  team: TeamInfoInterface[] = [];
   loaded = false;
 
   constructor( private http: HttpClient) {
 
      console.log('Info page service');
-
-     this.http.get('assets/data/data-page.json')
-       .subscribe( (resp: PageInfoInterface) => {
-         this.loaded = true;
-         this.info = resp;
-       });
+     this.loadInfo();
+     this.loadTeam();
+  }
+  private loadInfo(){
+    this.http.get('assets/data/data-page.json')
+      .subscribe( (resp: PageInfoInterface) => {
+        this.loaded = true;
+        this.info = resp;
+        console.log(resp);
+      });
+  }
+  private loadTeam(){
+      this.http.get(' https://portfolio-751cd.firebaseio.com/team.json')
+        .subscribe( (resp: TeamInfoInterface []) => {
+          this.loaded = true;
+          this.team = resp;
+          console.log(resp);
+        });
   }
 }
